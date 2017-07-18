0133b716e0957711db7d72c5eb391409a17d0a1a
sunil <sunilganiger@gmail.com>
2017-07-17 23:16:46 -0500

    first project setrup

---
+++ es6/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const sentences = [{subject: 'javascript', verb: 'is', object: 'great'},
+ {subject: 'elephant', verb: 'are', object: 'large'}];
+
+ function say({subject, verb, object}){
+  console.log(`${subject} ${verb} ${object}`);
+ }
+
+ for(let s of sentences){
+     say(s);
+ }
