c5f79052d3eee0b3461a46c0e5a94be9d3a125a6
sunil <sunilganiger@gmail.com>
2017-08-09 22:40:51 -0500

    working simulated game

--- es6/game.js
+++ es6/game.js
@@ -2,47 +2,60 @@
 
 let board = ['crown', 'anchor', 'heart', 'spade', 'club', 'diamond'];
 function rand(m, n) {
-    return Math.floor(Math.random() * (m - n + 1));
+    return Math.floor(Math.random() * (n - m + 1));
 }
 
 function randomFace() {
-    return board[rand(1, 6)];
+    let randValue = rand(1, 6);
+    return board[randValue == 0 ? randValue : randValue - 1];
 }
 
 function placeBets(fundsAvailable) {
-    let betFund = rand(1, fundsAvailable);
+    let betFund = fundsAvailable == 1 ? fundsAvailable : rand(1, fundsAvailable);
+
+    let totalbetValue = betFund;
     let bets = {};
     for (let i = 0; i < board.length; i++)
         bets[board[i]] = 0;
 
     if (betFund == 7) {
-        nets.crown = fundsAvailable;
-        fundsAvailable = 0;
+        bets.heart = fundsAvailable;
+        totalbetValue = fundsAvailable;
     }
     else {
+
         do {
-            var randFund = rand(1, betFund);
+            let remainingFund = betFund > 1 ? rand(1, betFund) : betFund;
             var randFace = randomFace();
-            bets[randFace] = bets[randFace] + randFund;
-            betFund = betFund - randFund;
-            fundsAvailable = fundsAvailable - randFund;
-        } while (betFund > 0)
+            bets[randFace] = bets[randFace] + remainingFund;
+            betFund = betFund - remainingFund;
+        } while (betFund)
     }
-    return bets;
+    return { betPlaced: bets, totalBetValue: totalbetValue };
 }
 
 function rollDice() {
     return randomFace();
 }
 
-function play(totalFunds) {
-    let bets = placeBets(totalFunds);
-    dice1 = rollDice();
-    dice2 = rollDice();
-    dice3 = rollDice();
+function playRound(funds) {
+    let bets = placeBets(funds);
+    console.log(`total bets placed ${bets.totalBetValue}`)
+    funds = funds - bets.totalBetValue;
+    console.log(`funds available: ${funds}`)
+    let dice1 = rollDice();
+    let dice2 = rollDice();
+    let dice3 = rollDice();
 
-    betMoneyWon = bets[dice1] + bets[dice2] + bets[dice3];
-    return betsWon;
+    let betMoneyWon = bets.betPlaced[dice1] + bets.betPlaced[dice2] + bets.betPlaced[dice3];
+    console.log(`won ${betMoneyWon}`)
+    return funds + betMoneyWon;
 }
 
-
+let totalFunds = 50;
+let round = 0;
+while (totalFunds && totalFunds < 100) {
+    round++;
+    console.log(`now playing round ${round}`);
+    totalFunds = playRound(totalFunds);
+}
